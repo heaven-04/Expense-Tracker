98739efa9f022f5d5c0b68dc9641d0e8ac22ddbb
Mohamed Ayoub Dababi <mohamedayoub.dababi@enicar.ucar.tn>
2026-08-21 02:02:29 +0100

    update : email field unique in user :)
    Added unique constraint to the email field in the User model.

--- models/models.py
+++ models/models.py
@@ -8,7 +8,7 @@ class User(Base) :
     __tablename__ = "users"
 
     id = Column(Integer ,nullable=False ,primary_key=True)
-    email = Column(String , nullable=False)
+    email = Column(String , nullable=False , unique=True)
     pwd = Column(String , nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),nullable=False,server_default=text('now()'))
     expenses = relationship("Expense", back_populates="owner")
